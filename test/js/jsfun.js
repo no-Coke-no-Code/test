f71d4166b81c1295150453949824db56a777504e
no-Coke-no-Code <1456751226@qq.com>
2018-04-07 16:54:13 +0800

    fitst commit

---
+++ test/js/jsfun.js
@@ -0,0 +1,19 @@
+/**
+ * Created by MY computer on 2017/9/7.
+ */
+function dianji(obj)
+{
+    obj.style.backgroundColor="#1ec5e5";
+}
+function songkai(obj)
+{
+    obj.style.backgroundColor="#8b8b8b";
+}
+function huaguo(obj)
+{
+    obj.style.backgroundColor="gold";
+}
+function likai(obj)
+{
+    obj.style.backgroundColor="#8b8b8b";
+}
